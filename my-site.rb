2e709e0c69da21d47bbad729398a2b3bcacb29d5
Elsa <elsa.moluf@gmail.com>
2015-06-08 15:47:02 -0700

    Word field is stored in params, returns word on page.

--- my-site.rb
+++ my-site.rb
@@ -8,4 +8,20 @@ class MySite < Sinatra::Base
     erb :score
   end
 
+  post "/" do
+    @word = params[:word]
+    # we eventually want to do this below
+    # @score = params[:scrabble][:score]
+    erb :score
+  end
+
 end
+
+# params hash
+# {
+#   scrabble: {
+#     word: "word input",
+#     score: "score output"
+#   }
+#
+# }
