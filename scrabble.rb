2d67d735cdf4e39d6b1151452a263f846a68edd7
Brenna <brennaleker@gmail.com>
2015-06-08 15:39:14 -0700

    working on breakfast

--- scrabble.rb
+++ scrabble.rb
@@ -1,11 +1,22 @@
-require "sinatra"
-require "sinatra/reloader"
+  require_relative 'score/scrabble_score'
+  require "sinatra"
+  require "sinatra/reloader"
 
-class Scrabble < Sinatra::Base
-  register Sinatra::Reloader
+  class Scrabble < Sinatra::Base
+    register Sinatra::Reloader
 
-  get "/" do
-    erb :index
+    get "/" do
+      erb :index
+    end
+
+    get "/score" do
+      erb :score
+    end
+
+    post "/score" do
+      @word = params[:word]
+      @score = params[:score]
+      erb :score
+    end
+
   end
-
-end 
