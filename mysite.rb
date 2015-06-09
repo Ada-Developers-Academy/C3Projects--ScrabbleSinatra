f75fb79ffe4df6e84ea1ad11958c6847d1238dce
Ashley Watkins <AWatkins31@gmail.com>
2015-06-09 16:01:52 -0700

    Adds home page + link to scoring page

--- mysite.rb
+++ mysite.rb
@@ -6,10 +6,14 @@ class MySite <Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
+    erb :index
+  end
+
+  get "/score" do
     erb :score
   end
 
-  post "/" do
+  post "/score" do
     word_array = params[:words].split(" ")
     score_method = params[:score_method]
     display_array = ScrabbleSinatra::ScrabbleSinatra.scoring(word_array, score_method)
