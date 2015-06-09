fc3b91773ad55402edb563149504bc82035d506b
Loren Wang <wang131.131@gmail.com>
2015-06-09 13:54:23 -0700

    moar

--- scrabble.rb
+++ scrabble.rb
@@ -6,13 +6,25 @@ class ScrabbleSite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
+    @letters = []
     erb :score
   end
 
   post "/" do
     @word  = params[:word]
     @score = Scrabble::Scrabble.score(@word)
+    @letters  = @word.chars
     erb :score
   end
+
+  # get "/werdz" do
+  #
+  #   erb :werdz
+  # end
+  #
+  # post "/werdz" do
+  #
+  #   erb :werdz
+  # end
 
 end
