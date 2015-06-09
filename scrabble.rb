b5f22562ae1d1fa994c947f200f3da0eeb3b3131
Brenna <brennaleker@gmail.com>
2015-06-09 14:16:03 -0700

    finished breakfast, refactored and added style

--- scrabble.rb
+++ scrabble.rb
@@ -13,10 +13,22 @@
       erb :score
     end
 
+    def output_score
+      score_line = ""
+      if @word.nil? == false
+        @score_line = "The word '#{@word}' scores #{@score} points."
+      else
+        @score_line = score_line
+      end
+    end
+
+    # posting a message only if there is a score available
     post "/score" do
       @word = params[:word]
       @score = Scrabble::Scrabble.score(@word)
+      output_score
       erb :score
     end
+
 
   end
