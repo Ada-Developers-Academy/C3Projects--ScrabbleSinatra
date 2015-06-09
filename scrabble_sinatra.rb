6d68c792054a57d88ce0a827426a1f7ca6079165
Shanna Chau <shannachau@gmail.com>
2015-06-09 13:46:57 -0700

    Added score_word_by_letter method

--- scrabble_sinatra.rb
+++ scrabble_sinatra.rb
@@ -15,7 +15,13 @@ class ScrabbleSinatra < Sinatra::Base
 
   post "/score" do
 
+    @word = params[:word][0]
     @score = Scrabble::Score.score(params[:word][0])
+
+    @letter_score = Scrabble::Score.score_word_by_letter(params[:word][0])
+
+
+
 
     erb :score
   end
