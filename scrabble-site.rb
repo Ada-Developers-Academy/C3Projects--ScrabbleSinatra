36142b378e734d6577f027878db41d8e881b80cb
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-09 15:33:40 -0700

    moved scored word logic into partial

--- scrabble-site.rb
+++ scrabble-site.rb
@@ -16,14 +16,9 @@ class ScrabbleSite < Sinatra::Base
   end
 
   post '/score' do
+    @title = "Score"
     @word = params[:word]
     @score = Scrabble.score(@word)
-    # @words = params[:list_of_words].split(" ")
-    # @scored_words = {}
-    # @words.each do |word|
-    #   @scored_words[word] = Scrabble.score(word)
-    # end
-    @title = "Score"
     erb :score
   end
 
