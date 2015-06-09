edeb7637caa78ddde5801267a973301044a71609
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-06-09 16:01:07 -0700

    lunch WIP.

--- ScrabbleSinatra.rb
+++ ScrabbleSinatra.rb
@@ -18,7 +18,10 @@ class ScrabbleSinatra < Sinatra::Base
     # Using instance variable because refering to @word on score.erb.
     # If local variable, score.erb is outside the scope.
     @score = Scrabble::Scrabble.score(@word)
+    @letters = Scrabble::Scrabble.letter_array(@word)
+    @letter_scoring = Scrabble::Scrabble.letter_scoring(@letters)
     erb :score
+
   end
 
   get "/multi_score" do
@@ -26,9 +29,18 @@ class ScrabbleSinatra < Sinatra::Base
   end
 
   post "/multi_score" do
+    @words_hash = {}
     @words = params[:words]
     @word_array = Scrabble::Scrabble.split_words(@words)
-    @score = Scrabble::Scrabble.score(@words)
+    # @score = Scrabble::Scrabble.score(@words)
+
+    # for each word in the @word_array, get letter and letter scores into individual arrays
+    @word_array.each do |word|
+      @letters = Scrabble::Scrabble.letter_array(word)
+      @letter_scoring = Scrabble::Scrabble.letter_scoring(@letters)
+      @words_hash[word] = [@letters, @letter_scoring]
+    end
+
     erb :multi_score
   end
 
